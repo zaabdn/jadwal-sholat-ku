5b893961a5ebfe97a52cb5073faabff235d1d264
AzureAD\ZainalAbidin <zainal.abidin@biposervice.com>
2024-08-08 15:23:25 +0700

    add color

---
+++ src/utils/colors.ts
@@ -0,0 +1,9 @@
+export const COLORS = {
+  PRIMARY: "#528974",
+  SECONDARY: "#86bfa8",
+  THIRD: "#50aaff",
+  BACKGROUND: "#F7F8FB",
+  DARK_BACKGROUND: "#000000",
+  BLACK: "#2C3A47",
+  WHITE: "#FFFFFF",
+};
